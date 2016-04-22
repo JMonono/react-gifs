dbe0490917b6c0d22319dc25201565b518eebc3e
JMonono <julian.monono@gmail.com>
2016-04-22 15:13:12 +0100

    Added modal

---
+++ src/components/GifModal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const GifModal = (props) => {
+  if (!props.selectedGif) {
+    return <div></div>;
+  }
+  return (
+    <Modal>
+      <div className="gif-modal">
+        <img src={props.selectedGif.images.original.url } />
+          <p><strong>Source:</strong> <a href={ props.selectedGif.source }>{ props.selectedGif.source }</a></p>
+          <p><strong>Rating:</strong> { props.selectedGif.rating }</p>
+
+          <button onClick={ () => props.onRequestClose() }>close</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Modal;
